dfe7e547310e390d388c53ea30df2ceb05984bed
eduardocms06 <eduardocatalaocamargo@gmail.com>
2026-08-20 12:49:51 -0300

    Adicionado a rota para chamar a API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from app.api.routes.chat import router as chat_router
 from app.core.config import settings
 
 app = FastAPI(
@@ -7,6 +7,7 @@ app = FastAPI(
     description="API do chatbot inteligente para WhatsApp.",
     version="0.1.0"
 )
+app.include_router(chat_router, prefix="/api/v1")
 
 @app.get("/")
 def home():
@@ -14,3 +15,4 @@ def home():
         "message": "Syntra Chatbot API está funcionando!",
         "database": settings.DATABASE_URL
     }
+
