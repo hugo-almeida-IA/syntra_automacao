6e0bbe501525033f101272299a0040c0f66ada94
eduardocms06 <eduardocatalaocamargo@gmail.com>
2026-08-21 07:24:43 -0300

    Inclusão da rota webhook

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.routes.chat import router as chat_router
+from app.api.routes.webhook import router as webhook_router
 from app.core.config import settings
 
 app = FastAPI(
@@ -8,6 +9,7 @@ app = FastAPI(
     version="0.1.0"
 )
 app.include_router(chat_router, prefix="/api/v1")
+app.include_router(webhook_router, prefix = "/api/v1")
 
 @app.get("/")
 def home():
